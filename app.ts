f37b5490c6b79362b8d06e080f8834ee3fe50843
guymeiri <guymeiri@microsoft.com>
2022-09-20 13:56:36 +0300

    add bind decorator

--- app.ts
+++ app.ts
@@ -1,7 +1,23 @@
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+
+  return adjDescriptor;
+}
+
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById("project-input") as HTMLTemplateElement;
@@ -9,11 +25,31 @@ class ProjectInput {
 
     const importedNode = document.importNode(this.templateElement.content, true);
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
     this.attach();
+
+    this.titleInputElement = this.element.querySelector("#title") as HTMLInputElement;
+    this.descriptionElement = this.element.querySelector("#description") as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector("#people") as HTMLInputElement;
+
+    this.configure();
   }
 
   private attach() {
     this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+
+  @autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    const title = this.titleInputElement.value;
+    const description = this.descriptionElement.value;
+    const people = this.peopleInputElement.value;
+    console.log(title + description + people);
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler);
   }
 }
 
